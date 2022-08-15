4a3935d0787d50eece820c07baf1bcf86cacb558
Darren Harris <dazza@me.com>
2022-08-15 18:04:43 +0800

    chore: Initial commit

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,32 @@
 const { LernaProject } = require("lerna-projen");
-const project = new LernaProject({
+const { awscdk } = require('projen');
+
+const parentProject = new LernaProject({
   defaultReleaseBranch: "main",
   devDeps: ["lerna-projen"],
   name: "lp3",
+  independentMode: true
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
-project.synth();
+
+const constructone = new awscdk.AwsCdkConstructLibrary({
+  author: 'Darren Harris',
+  authorAddress: 'darren.harris@modis.com',
+  cdkVersion: '2.27.0',
+  defaultReleaseBranch: 'main',
+  name: 'constructone',
+  parent: parentProject,
+  outdir: 'constructone',
+  repository: {
+    type: 'npm',
+    url: 'https://github.com/Dazza65/pattern-library.git'
+  },
+  description: 'constructone description'
+});
+
+parentProject.addSubProject(constructone);
+
+parentProject.synth();
